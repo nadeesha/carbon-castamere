4ecb2cf105d8d586c9f3b916382aa4f67a5738db
Nadeesha Cabral <nadeesha.cabral@gmail.com>
2014-05-20 14:18:58 +0530

    relative paths

--- castamere.js
+++ castamere.js
@@ -3,16 +3,16 @@
 angular.module('carbon.castamere', ['ui.router'])
     .config(function($stateProvider) {
         $stateProvider.state('castamereHome', {
-        	url: '/castamere',
-            templateUrl: 'carbon_components/castamere/main.html',
+            url: '/castamere',
+            templateUrl: 'components/carbon-castamere/main.html',
             controller: 'castamereCtrl',
             navigationBar: {
-            	name: 'Catamere',
-            	glyphicon: 'asterisk',
-            	weight: 0
+                name: 'Catamere',
+                glyphicon: 'asterisk',
+                weight: 0
             }
         });
     })
     .controller('castamereCtrl', function ($scope) {
-		$scope.verse = 'And now the rains weep o\'er his hall and not a soul to hear.';
-	});
+        $scope.verse = 'And now the rains weep o\'er his hall and not a soul to hear.';
+    });
